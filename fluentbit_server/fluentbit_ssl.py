c6655d99fecbb586740b147488c02ba10cefd8d0
lmoulin <lmoulin@spartan-conseil.fr>
2020-12-17 17:43:30 +0100

    move context creation in constructor to prevent runtime error while cert/key do not exist

--- fluentbit_server/fluentbit_ssl.py
+++ fluentbit_server/fluentbit_ssl.py
@@ -1,16 +1,24 @@
 import ssl
+import os.path
 
 class FluentbitSSL:
     def __init__(self, key_file, crt_file, verify=False):
-        self._key_file = key_file
-        self._crt_file = crt_file
-        self._verify = verify
+        self._context = self.create_context(key_file, crt_file, verify)
+
+    def create_context(self, key_file, crt_file, verify):
+        if not os.path.isfile(key_file):
+            raise IOError("Key file {} doesn't exist".format(key_file))
+
+        if not os.path.isfile(crt_file):
+            raise IOError("Certificat file {} doesn't exist".format(crt_file))
+
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        context.verify_mode = ssl.CERT_REQUIRED if verify else ssl.CERT_OPTIONAL
+        context.load_cert_chain(crt_file, key_file)
+
+        return context
 
     def wrap(self, connection):
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-        context.verify_mode = ssl.CERT_REQUIRED if self._verify else ssl.CERT_OPTIONAL
-        context.load_cert_chain(self._crt_file, self._key_file)
-
-        output = context.wrap_socket(connection, server_side=True)
+        output = self._context.wrap_socket(connection, server_side=True)
 
         return output
